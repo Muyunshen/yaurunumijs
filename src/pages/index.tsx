b9e33dec6441b81d419fb6ff41357c9e12b29d9e
Muyunshen <1429299430@qq.com>
2022-06-07 19:22:59 +0800

    #1

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index226</h1>
+      <h2>暮云深</h2>
+    </div>
+  );
+}
